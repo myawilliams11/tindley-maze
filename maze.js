6f3551b1329778d26b2792feeedabce964643973
Edwin <edwin@kenzie.academy>
2018-03-16 12:26:33 -0400

    Finished extra hard

--- maze.js
+++ maze.js
@@ -2,106 +2,16 @@ const mazeDiv = document.getElementById("mazeDiv");
 const avatar = document.getElementById("avatar");
 
 // Size of the squares in the grid, in pixels.
-const delta = 33;
+const squareSize = 33;
 
-// Coordinates of the player's avatar.
-var avatarRow, avatarCol;
-
-// Initial scan of the map
-// Create all the visual elements (divs)
-// The static parts of the map will be a grid of flex positioned divs.
-// They will get created here, and never modified again.
-// The moving player will be an absolutely positioned div on top.
-for(var row = 0; row < map.length; row++) {
-    var rowStr = map[row];
-    var rowDiv = document.createElement("div");
-    rowDiv.className = "row";
-    
-    for(var i = 0; i < rowStr.length; i++) {
-        const cellClass = rowStr[i];
-        var cellDiv = document.createElement("div");
-        cellDiv.className = "cell " + cellClass;
-        if(cellClass === "S") {
-            avatarCol = i;
-            avatarRow = row;
-        }
-        if(cellClass === "S" || cellClass === "F") {
-            cellDiv.innerHTML = cellClass;
-        }
-        rowDiv.appendChild(cellDiv);
-    }
-    mazeDiv.appendChild(rowDiv);
-}
-
-// Update the coordinates of the player's avatar.
-// Add a class indicating which animation to show.
-// Set a timeout to remove the animation class after the animation completes.
-function redrawAvatar(anim) {
-    avatar.className = "";
-    avatar.style.top = avatarRow*delta + "px";
-    avatar.style.left = avatarCol*delta + "px";
-    avatar.className = anim;
-    setTimeout(function() {
-		avatar.classList.remove(anim);
-	}, 100);
-}
-
-redrawAvatar("slideRight");
-
-// Move the player's avatar in the specified direction
-// dRow is the desired change in row (-1, 0, or +1)
-// dCol is the desired change in column (-1, 0, or +1)
-function move(dRow, dCol, anim) {
-    // "avatarRow" and "avatarCol" are the current row and column the player is on.
-    // The way this function should work, is that dRow is used to determine how many
-    // rows to move the player by, and dCol is used to determine how many columns to move the player by.
-    // Calculate the coordinates the player wants to move to.
-
-
-    // Using avatarRow and dRow, define a variable named "destRow" (where the
-    // player should move vertically). You'll need to replace "undefined" to do
-    // so.
-
-    // DEFINE destRow HERE
-
-    // Using avatarCol and dCol, define a variable named "destCol" (where the player should move
-    // vertically). You'll need to replace "undefined" to do so.
-
-    // DEFINE destCol HERE
-
-    // Finally, define a variable named "destCell" which is the cell at
-    // (destRow, destCol). Remember that to access items in a one-dimentional
-    // array, you'll need to do something like "arr[row]", where "arr" is the
-    // name of your array variable. For a two-dimensional array (like our map),
-    // you'll need two pairs of brackets like this: arr[row][col]. 
-
-    // DEFINE destCell HERE
-
-    // Keep in mind that the name of the array you want to access here is "map"
-
-    // DO NOT EDIT BELOW THIS LINE --------------------------------------------/
-
-    // Check that it is within the bounds of the map, and not a wall.
-    if(destCell && destCell !== "W") {
-        avatarRow += dRow;
-        avatarCol += dCol;
-        redrawAvatar(anim);
-        if(destCell === "F") {
-            document.getElementById("youWonDiv").style.display = "block";
-            var audio = new Audio('wow.mp3');
-            audio.play();
-        }
-    }
-}
+// Using the map in map.js, create a maze game. "W" represents walls, "S"
+// represents the player's start position, "F" represents the finish position
 
 document.addEventListener('keydown', (event) => {
-    // keyName will be one of "ArrowDown", "ArrowUp", "ArrowLeft" or "ArrowRight"
     const keyName = event.key;
 
-    // move takes 3 arguments: a row offset, a column offset, and an animation.
-    // For example move(1, 0, "slideDown") would move the player 1 square down,
-    // 0 squares to the right, and use a "slideDown" animation when moving the player.
+    // Implement logic to move the avatar around when the user presses an arrow
+    // key. keyName will be one of "ArrowLeft", "ArrowRight", "ArrowUp",
+    // "ArrowDown".
 
-    // Write some logic to check the value of "keyName" and call "move()" with
-    // the proper arguments
 });
